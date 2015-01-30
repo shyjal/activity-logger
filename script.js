55ef9c1517d1a2f8834f5a9c4694f2484c931014
Shyjal Raazi <shyjal99@gmail.com>
2015-01-30 16:35:31 +0530

    commit

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ var app = {
 	init: function() {
 		document.getElementById('acitvity_input').addEventListener('submit', function(e) {
 			e.preventDefault();
-
-			document.getElementById("infobox").style.display = "block";
-
 			var activityname = document.getElementById("activity_name").value;
 			new Activity().create(activityname);
 		});
